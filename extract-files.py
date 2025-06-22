93114f301edaa3390dd5de3b70408ff080172d2b
blackshibe <kacperszczepan@tutanota.com>
2025-06-22 23:11:01 +0200

    sm8550-common: shim fingerprint

--- extract-files.py
+++ extract-files.py
@@ -115,6 +115,10 @@ blob_fixups: blob_fixups_user_type = {
     .add_needed(
         'libbase_shim.so'
     ),
+    'vendor/bin/hw/android.hardware.biometrics.fingerprint@2.1-service-rbs': blob_fixup()
+    .add_needed(
+        'libbase_shim.so'
+    )
 }  # fmt: skip
 
 module = ExtractUtilsModule(
